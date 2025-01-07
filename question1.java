3975a56e237ff843a2acb84c4fa9bf0290bbe3bc
trishab2005 <trishabasak2005@gmail.com>
2025-01-07 22:38:46 +0530

    Add files via upload

---
+++ question1.java
@@ -0,0 +1,20 @@
+   // Author trisha
+
+public class question1 {
+
+    public static void main(String ...args) {
+        printNumbers();                       //Main method
+    }
+
+     static void printNumbers() {
+        for (int i = 1; i <= 7; i++) {
+            if (i == 3) {                      //Method for printing the numbers
+                System.out.println("I am 3");
+            } else if (i == 5) {
+                System.out.println("I am 5");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ question4.java
@@ -0,0 +1,15 @@
+// Author trisha
+
+class question4 {
+    public static void main(String ...args) {
+        Desc();                                      //Main method
+    }
+
+     static void Desc() {
+        int x= 20;                          //method for calculating 20 to 4
+        while (x >= 4) {
+            System.out.println(x);
+            x--;
+        }
+    }
+}
